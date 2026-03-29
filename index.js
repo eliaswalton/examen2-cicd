e8951eec4ce11cde161bbf8bae2d7a871822a622
Elias Walton <elias_walton@hotmail.com>
2026-03-28 18:52:26 -0600

    Dockerfile corregido ACTIVIDAD A1

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hola Mundo - Examen 2 CI/CD\n');
+});
+
+server.listen(PORT, () => {
+  console.log('Servidor corriendo en puerto ' + PORT);
+});
